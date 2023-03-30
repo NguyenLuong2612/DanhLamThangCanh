d32c203fbee32d13c825325f6f985bc5c22d5619
phongLee3112 <116968082+phongLee3112@users.noreply.github.com>
2023-03-30 21:31:00 +0700

    add layout bg

---
+++ app/src/main/java/com/example/danhlamthangcanh/Background_delay.java
@@ -0,0 +1,24 @@
+package com.example.danhlamthangcanh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Background_delay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background_delay);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), Activity_One.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
