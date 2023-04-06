be6a9e9cac1a49084ff75d4cbfe03c369a4c50c1
phongLee3112 <116968082+phongLee3112@users.noreply.github.com>
2023-04-06 21:22:49 +0700

    fragment

--- app/src/main/java/com/example/danhlamthangcanh/HomeActivity.java
+++ app/src/main/java/com/example/danhlamthangcanh/HomeActivity.java
@@ -1,18 +1,34 @@
 package com.example.danhlamthangcanh;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class HomeActivity extends AppCompatActivity {
     Button btn_miennam, btn_mienbac, btn_mientrung;
+    BottomNavigationView mnButtom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mnButtom = findViewById(R.id.navMenu);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Main");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        mnButtom.setOnItemSelectedListener(getListener());
+
         btn_mienbac = (Button) findViewById(R.id.btn_mienbac);
         btn_mientrung = (Button) findViewById(R.id.btn_mientrung);
         btn_miennam = (Button) findViewById(R.id.btn_miennam);
@@ -37,5 +53,40 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    void loadFragment (Fragment fmNew){
+        FragmentTransaction fmTran = getSupportFragmentManager().beginTransaction();
+        fmTran.replace(R.id.main_fragment, fmNew);
+        fmTran.addToBackStack(null);
+        fmTran.commit();
+    }
+
+    @NonNull
+    private NavigationBarView.OnItemSelectedListener getListener() {
+        return new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fmNew;
+                switch (item.getItemId()) {
+                    case R.id.mnHome:
+                        getSupportActionBar().setTitle(item.getTitle());
+                        fmNew = new HomeFragment();
+                        loadFragment(fmNew);
+                        return true;
+                    case R.id.mnList:
+                        getSupportActionBar().setTitle(item.getTitle());
+                        fmNew = new ListFragment();
+                        loadFragment(fmNew);
+                        return true;
+                    case R.id.mnSetting:
+                        getSupportActionBar().setTitle(item.getTitle());
+                        fmNew = new SettingFragment();
+                        loadFragment(fmNew);
+                        return true;
+                }
+                return true;
+            }
+        };
     }
 }
